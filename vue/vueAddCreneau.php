035cee409a7ce98b734f5744f569fd69950e04ff
unknown <romain.provost2@gmail.com>
2021-01-28 13:55:43 +0100

    modif horaires

--- vue/vueAddCreneau.php
+++ vue/vueAddCreneau.php
@@ -11,10 +11,38 @@
 	<table><tr><td>
 
 	<p><label for="CRE_DATE"> Date </label> <input id="CRE_DATE" type="date" name="CRE_DATE" size="50px" maxlength="20"  class= "instyle" /></p>
-	<p><label for="CRE_HEUREDEB"> Heure de début </label> <input id="CRE_HEUREDEB" type="time" name="CRE_HEUREDEB" size="50px" maxlength="20" class= "instyle"/></p>
-	<p><label for="CRE_SALLE"> Salle </label> <input id="CRE_SALLE" type="text" name="CRE_SALLE" required pattern="[A-Z][0-9]{4}" size="50px" maxlength="5"/></p>
-	<p><label for="CRE_HEUREFIN"> Heure de fin </label> <input id="CRE_HEUREFIN" type="time" name="CRE_HEUREFIN" size="50px" maxlength="20"  /></p>
-
+	<p><label for="CRE_HEUREDEB">Heure de début</label><select name="CRE_HEUREDEB" id="CRE_HEUREDEB" type="time">
+      <option value="8:15">8:15</option>
+      <option value="9:10">9:10</option>
+      <option value="10:05">10:05</option>
+	  <option value="10:20">10:20</option>
+      <option value="11:15">11:15</option>
+      <option value="12:10">12:10</option>
+	  <option value="13:05">13:05</option>
+      <option value="14:00">14:00</option>
+      <option value="14:55">14:55</option>
+	  <option value="15:50">15:50</option>
+      <option value="16:05">16:05</option>
+      <option value="17:00">17:00</option>
+	  <option value="17:55">17:00</option>
+	</select></p>
+	<p><label for="CRE_SALLE"> Salle </label> <input id="CRE_SALLE" type="text" name="CRE_SALLE" required pattern="[A-Z]{1}[0-9]{3}" size="50px" maxlength="5"/></p>
+	<p><label for="CRE_HEUREFIN">Heure de début</label><select name="CRE_HEUREFIN" id="CRE_HEUREFIN" type="time">
+      <option value="8:15">8:15</option>
+      <option value="9:10">9:10</option>
+      <option value="10:05">10:05</option>
+	  <option value="10:20">10:20</option>
+      <option value="11:15">11:15</option>
+      <option value="12:10">12:10</option>
+	  <option value="13:05">13:05</option>
+      <option value="14:00">14:00</option>
+      <option value="14:55">14:55</option>
+	  <option value="15:50">15:50</option>
+      <option value="16:05">16:05</option>
+      <option value="17:00">17:00</option>
+	  <option value="17:55">17:00</option>
+	</select></p>
+	
 	<div class="ui-widget">
 	<p><label for="STA_ELEMIN"> Nombre d'élève min </label> <input type="number" id="ele_min" name="STA_ELEMIN" min="1" max="3"></p>
 	<br>
